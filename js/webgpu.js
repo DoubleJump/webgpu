c71c3564f664260f876be7a422ebb82a7f750028
Gareth Battensby <gareth@parall.ax>
2019-05-23 12:01:24 +0100

    ...

--- js/webgpu.js
+++ js/webgpu.js
@@ -19,9 +19,55 @@ function Uniform_Buffer()
     });
 }
 
+/*
+Vertex Fomat
+"uchar2",
+"uchar4",
+"char2",
+"char4",
+"uchar2norm",
+"uchar4norm",
+"char2norm",
+"char4norm",
+"ushort2",
+"ushort4",
+"short2",
+"short4",
+"ushort2norm",
+"ushort4norm",
+"short2norm",
+"short4norm",
+"half2",
+"half4",
+"float",
+"float2",
+"float3",
+"float4",
+"uint",
+"uint2",
+"uint3",
+"uint4",
+"int",
+"int2",
+"int3",
+"int4"
+*/
+
+/*
+Index Format
+"uint16",
+"uint32"
+*/
+
+/*
+Step Mode
+"vertex",
+"instance"
+*/
+
 function Vertex_Buffer(data, usage)
 {
-	var r = _ctx.device.createBuffer(
+	var r = _GPU.device.createBuffer(
 	{
 		size: data.byteLength,
 		usage: usage,
@@ -40,21 +86,6 @@ function Mesh()
 	return; r;
 }
 
-function Shader(vertex_src, fragment_src)
-{
-	var r = {};
-	r.vertex_src = vertex_src;
-	r.fragment_src = fragment_src;
-	r.vertex_module;
-	r.fragment_module;
-	return r;
-}
-
-function bind_shader(shader)
-{
-
-}
-
 function Mipmap(width, height, data)
 {
 	var r = {};
@@ -64,15 +95,99 @@ function Mipmap(width, height, data)
 	return r;
 }
 
-function Sampler(down, up)
+/*
+// SAMPLERS //
+
+Sampler tiling
+
+"clamp-to-edge",
+"repeat",
+"mirror-repeat"
+*/
+
+/*
+Sampler Filtering
+"nearest",
+"linear"
+*/
+
+function Sampler(params)
 {
-	return _ctx.device.createSampler(
-	{
+    return _GPU.device.createSampler(params);
+}
+
+function default_sampler()
+{
+    return _GPU.device.createSampler(
+    {
+        wrap_u: u,
+        wrap_v: v,
+        wrap_w: w,
         magFilter: up,
         minFilter: down,
-        //more options??
+        mipmapFilter: mipmap,
+        compareFunction: compare
     });
 }
+
+/*
+Texture Formats
+TODO: pick out common ones an rename
+
+"r8unorm",
+"r8unorm-srgb",
+"r8snorm",
+"r8uint",
+"r8sint",
+"r16unorm",
+"r16snorm",
+"r16uint",
+"r16sint",
+"r16float",
+"rg8unorm",
+"rg8unorm-srgb",
+"rg8snorm",
+"rg8uint",
+"rg8sint",
+"b5g6r5unorm",
+"r32uint",
+"r32sint",
+"r32float",
+"rg16unorm",
+"rg16snorm",
+"rg16uint",
+"rg16sint",
+"rg16float",
+"rgba8unorm",
+"rgba8unorm-srgb",
+"rgba8snorm",
+"rgba8uint",
+"rgba8sint",
+"bgra8unorm",
+"bgra8unorm-srgb",
+"rgb10a2unorm",
+"rg11b10float",
+"rg32uint",
+"rg32sint",
+"rg32float",
+"rgba16unorm",
+"rgba16snorm",
+"rgba16uint",
+"rgba16sint",
+"rgba16float",
+"rgba32uint",
+"rgba32sint",
+"rgba32float",
+"depth32float",
+"depth32float-stencil8"
+*/
+
+/*
+Texture Dimensions
+"1d",
+"2d",
+"3d"
+*/
 
 function Texture()
 {
@@ -81,6 +196,7 @@ function Texture()
 	r.width;
 	r.height;
 	r.depth;
+    r.dimension;
 	r.format;
 	r.data;
     r.num_mipmaps;
@@ -90,43 +206,150 @@ function Texture()
 function bind_texture(t)
 {
 	var usage = ;
-	var format = "";
-	var dimension = "2d";
-	//if(t.format is whatever) dimension = "3d";
 
-    t.id = _ctx.device.createTexture(
+    t.id = _GPU.device.createTexture(
     {
 	    size: {width: t.width, height: t.height, depth: t.depth},
 	    arrayLayerCount: 1, //dunno
 	    mipLevelCount: t.num_mipmaps,
 	    sampleCount: 1,
-	    dimension: dimension,
-	    format: format,
+	    dimension: t.dimension,
+	    format: t.format,
 	    usage: usage
 	});
 }
 
-function pipeline(shader)
+
+/*
+Front Face
+"ccw",
+"cw"
+*/
+
+/*
+Cull Mode
+"none",
+"front",
+"back"
+*/
+
+function Shader(vertex_src, fragment_src)
 {
-    var pipelineLayout = device.createPipelineLayout({ bindGroupLayouts: [uniformsBindGroupLayout] });
+    var r = {};
+    r.vertex_src = vertex_src;
+    r.fragment_src = fragment_src;
+    r.vertex_module;
+    r.fragment_module;
+    return r;
+}
+
+function bind_shader(shader)
+{
+    shader.vertex_module = _GPU.device.createShaderModule(compile(shader.vertex_src));
+    shader.fragment_module = _GPU.device.createShaderModule(compile(shader.fragment_src));
+}
+
+/*
+Topology
+"point-list",
+"line-list",
+"line-strip",
+"triangle-list",
+ "triangle-strip"
+*/
+
+
+// could have a list of common raster type, mesh types etc??
+
+// Raster State
+
+default: 
+{
+    frontFace: 'ccw'
+    cullMode = 'back';
+    depthBias = 0; //i32
+    depthBiasSlopeScale = 0; //f32
+    depthBiasClamp = 0; //f32
+};
+
+// Depth Stencil State
+
+default:
+{
+    depthWriteEnabled: true,
+    depthCompare: "less",
+    format: "depth32float-stencil8",
+    stencilFront: {},
+    stencilBack: {},
+},
+
+// Color State
+/*
+interface GPUColorWriteBits 
+{
+    const u32 NONE = 0;
+    const u32 RED = 1;
+    const u32 GREEN = 2;
+    const u32 BLUE = 4;
+    const u32 ALPHA = 8;
+    const u32 ALL = 15;
+};
+
+Blend Mode
+"zero",
+"one",
+"src-color",
+"one-minus-src-color",
+"src-alpha",
+"one-minus-src-alpha",
+"dst-color",
+"one-minus-dst-color",
+"dst-alpha",
+"one-minus-dst-alpha",
+"src-alpha-saturated",
+"blend-color",
+"one-minus-blend-color"
+
+Blend Function
+"add",
+"subtract",
+"reverse-subtract",
+"min",
+"max"
+
+GPUBlendDescriptor {
+    GPUBlendFactor srcFactor = "one";
+    GPUBlendFactor dstFactor = "zero";
+    GPUBlendOperation operation = "add";
+};
+*/
+
+default, inverse, overlay etc
+{
+    srcFactor: "one,
+    dstFactor: "zero",
+    operation: "add",
+}
+
+// Blends
+default:
+{
+    format: "bgra8unorm",
+    alphaBlend: {},
+    colorBlend: {},
+    writeMask: GPUColorWriteBits.ALL
+}
+
+
+
+
+function pipeline(shader, raster, depth_stencil, blend)
+{
+    var layout = _GPU.device.createPipelineLayout({ bindGroupLayouts: [uniformsBindGroupLayout] });
     
-    var pipeline = device.createRenderPipeline(
+    var pipeline = _GPU.device.createRenderPipeline(
     {
-        layout: pipelineLayout,
-        /*
-        vertexStage: {
-            module: device.createShaderModule({
-                code: Utils.compile("v", vertexShaderGLSL),
-            }),
-            entryPoint: "main"
-        },
-        fragmentStage: {
-            module: device.createShaderModule({
-                code: Utils.compile("f", fragmentShaderGLSL),
-            }),
-            entryPoint: "main"
-        },
-        */
+        layout: layout,
 
         vertexStage: 
         {
@@ -138,21 +361,14 @@ function pipeline(shader)
             module: shader.fragment_module, 
             entryPoint: "main"
         },
-        
-        //primitiveTopology: "triangle-list",
-        primitiveTopology: "triangle-list",
 
-        depthStencilState: 
-        {
-            depthWriteEnabled: true,
-            depthCompare: "less",
-            format: "depth32float-stencil8",
-            stencilFront: {},
-            stencilBack: {},
-        },
+        primitiveTopology: "triangle-list",
+        rasterizationState: raster,
+        depthStencilState: depth_stencil,
 
         // can we fetch these from the shader??
-        vertexInput: {
+        vertexInput: 
+        {
             indexFormat: "uint32",
             vertexBuffers: [{
                 stride: vertexSize,
@@ -170,17 +386,14 @@ function pipeline(shader)
                 }]
             }],
         },
-        rasterizationState: 
-        {
-            frontFace: 'ccw',
-            cullMode: 'back',
-        },
-        colorStates: [{
-            format: "bgra8unorm",
-            alphaBlend: {},
-            colorBlend: {},
-        }],
+        
+        colorStates: [blend],
+
+        sampleCount: 1, //AA
+
     });
+
+
 }
 
 function render_pass()
@@ -213,6 +426,12 @@ function uniform_group()
             });
 }
 
+/*
+Power Preference
+"low-power",
+"high-performance"
+*/
+
 async function WebGPU(canvas, options)
 {
 	if(!navigator.gpu)
@@ -222,8 +441,15 @@ async function WebGPU(canvas, options)
 	}
 
 	var r = {};
-	r.adapter = await navigator.gpu.requestAdapter();
-	r.device = await adapter.requestDevice({});
+	r.adapter = await navigator.gpu.requestAdapter(
+    {
+        powerPreference: "high-performance",
+    }); 
+	r.device = await adapter.requestDevice(
+    {
+        extensions: ,
+        limits: ,
+    }); 
 	r.ctx = canvas.getContext('gpupresent');
 
 	if(!r.adapter |! r.device |! r.ctx)
@@ -235,8 +461,9 @@ async function WebGPU(canvas, options)
     // double, triple buffer etc
 	r.swap_chain = r.ctx.configureSwapChain(
 	{
-		r.device, //no key?
-		format: "bgra8unorm", //give these better names
+		device: r.device,
+		format: "bgra8unorm", //GPUTextureFormat
+        //usage: GPUTextureUsage.OUTPUT_ATTACHMENT
     });
 
     r.uniform_groups = 
@@ -276,7 +503,7 @@ function render()
 	var r = _ctx;
 
     pass_descriptor.colorAttachments[0].attachment = r.swap_chain.getCurrentTexture().createDefaultView();
-    var cmd_encoder = _ctx.device.createCommandEncoder({});
+    var cmd_encoder = _GPU.device.createCommandEncoder({});
 
     //update uniforms
     uniformBuffer.setSubData(0, mvp);
